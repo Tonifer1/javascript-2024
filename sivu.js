2d5211dbad1cec9451a23a2a4c08e6a13ddcbd59
Toni Pekkala <toni.pekkala@hotmail.com>
2024-01-17 11:03:49 +0200

    Buttonien lisäys

---
+++ sivu.js
@@ -0,0 +1,11 @@
+//palvelut sivun näyttäminen 
+function palvelut(){
+    let palvelut = `<h2>Palvelumme</h2>
+    <ul>
+        <li>Eläinlääkäri</li>
+        <li>Päivähoito</li>
+        <li>Yöhoito</li>
+   </ul>
+   `
+   document.getElementById("sisältö").innerHTML = palvelut
+}
