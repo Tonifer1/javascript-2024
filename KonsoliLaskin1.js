8960cb8f285a19635c6f6c3120def25fb12886ef
Toni Pekkala <toni.pekkala@hotmail.com>
2024-09-26 22:05:51 +0300

    Kommentointeja lisätty

--- KonsoliLaskin1.js
+++ KonsoliLaskin1.js
@@ -1,15 +1,25 @@
 let summa = 0;
-//Määritellään process.stdin standardiksi input-objektiksi.
+
+//Määritellään process.stdin standardiksi input-objektiksi. Löytyy Javascriptistä
 var standard_input = process.stdin;
+
 //Asetetaan input-merkistö
 standard_input.setEncoding('utf-8');
+
 //Aloitetaan sovellus ja kirjoitetaan käyttäjälle ohjeita
 console.log("JavaScript-laskin, konsolisovellus (V0.1)");
 console.log('Sovellus suljetaan kirjoittamalla "exit"');
+
 //Perustetaan prosessi
 process.stdout.write("Anna ensimmäinen luku: ");
-//Seuraavaa rutiinia kutsutaan aina, kun käyttäjä kirjoittaa jotain ja/tai painaa enteriä. Silmukka?
+
+//Seuraavaa rutiinia kutsutaan aina, kun käyttäjä kirjoittaa jotain ja/tai painaa enteriä. 
 //(data) muuttuja = tallentaa käyttäjän antaman syötteen
+//Tapahtumakuuntelija (on-metodi): standard_input.on('data', function (data) { ... }) -rutiini määrittelee, 
+//mitä tapahtuu, kun käyttäjä syöttää jotain ja painaa Enter. Kun syöte tapahtuu, määritelty funktio suoritetaan.
+//Käytännössä tämä on tapahtumapohjainen lähestymistapa, joka toimii silmukan kaltaisesti. Node.js
+//tapahtumapohjaiset ominaisuudet tekevät tästä mahdollisen ilman perinteisten silmukoiden käyttöä.
+
 standard_input.on('data', function (data) {
 
 
@@ -28,4 +38,6 @@ standard_input.on('data', function (data) {
     //node .\KonsoliLaskin1.js
 
 });
+
+
 
--- KonsoliLaskin2.js
+++ KonsoliLaskin2.js
@@ -1,12 +1,17 @@
+//Ohjelmasta puuttuu kaikki required moduulit
+
 let kaava = '';//luodaan kaava muuttuja
-let tulos = 0; //luodaantulos muuttuja
+let tulos = 0; //luodaan tulos muuttuja
+
 //Määritellään process.stdin standardiksi input-objektiksi.
 var standard_input = process.stdin;
+
 //Asetetaan input-merkistö
 standard_input.setEncoding('utf-8');
+
 //Aloitetaan sovellus ja kirjoitetaan käyttäjälle ohjeita
-console.log("JavaScript-laskin, konsolisovellus (V0.1)");
 console.log("Sovellus suljetaan kirjoittamalla 'exit'");
+
 process.stdout.write("Anna laskukaava: ");
 
 //Seuraavaa rutiinia kutsutaan aina, kun käyttäjä kirjoittaa jotain ja/tai painaa enteriä .Tallentuu (data) olioon syöte.
@@ -16,10 +21,11 @@ standard_input.on('data', function (data) {
     if (data === 'exit\r\n'){
         console.log("Käyttäjä sulki laskimen");
         process.exit();
-    } else //Muussa tapauksessa lasketaan yhteen lukujen summia annetun laskukaavan perusteella
+    } 
+    else //Muussa tapauksessa lasketaan yhteen lukujen summia annetun laskukaavan perusteella
     {
-        kaava = data.trim();
-        tulos = eval(kaava);
+        kaava = data.trim();// data siirtyy kaavaan tässä ja trim poistaa tyhjät välilyönnit
+        tulos = eval(kaava);  //eval-funktio laskee annetun laskukaavan. Sisäänrakennettu funktio, joka arvioi merkkijonon ja palauttaa tuloksen.
         console.log(kaava + '=' + tulos);
         process.stdout.write("Anna uusi laskukaava: ");
     }
